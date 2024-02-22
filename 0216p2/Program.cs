539acfaad21ac12957660665be3aebe2e974e7ac
fintalev <112973569+fintalev@users.noreply.github.com>
2024-02-22 12:08:25 +0100

    f

--- 0216p2/Program.cs
+++ 0216p2/Program.cs
@@ -1,47 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0216p2
 {
     internal class Program
     {
-
         class OneChain
         {
             public int data;
             public OneChain next = null;
-        public OneChain(int value) 
-        {
-            data = value;
+
+            public OneChain(int value)
+            {
+                data = value;
+            }
         }
-        }
+
         static void Main(string[] args)
         {
             OneChain head = null;
 
-            OneChain node = new OneChain(1);
-            head = node;
+            
+            Add(ref head, new OneChain(1));
+            Add(ref head, new OneChain(2));
+            Add(ref head, new OneChain(3));
+            Add(ref head, new OneChain(4));
 
-            OneChain node2= new OneChain(2);
+           
+            PrintList(head);
 
-            head.next = node2;
+           
+            Remove(ref head, 2);
 
-            OneChain node3= new OneChain(3);
-
-            node2.next = node3;
-            node3.next = null;
-
-            OneChain item = head;
-            while (item!=null)
-            {
-                Console.WriteLine(item.data);
-                item = item.next;
-            }
+        
+            PrintList(head);
 
             Console.ReadKey();
+        }
+
+       
+        static void Add(ref OneChain head, OneChain newNode)
+        {
+            if (head == null || head.data >= newNode.data)
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+            else
+            {
+                OneChain current = head;
+                while (current.next != null && current.next.data < newNode.data)
+                {
+                    current = current.next;
+                }
+                newNode.next = current.next;
+                current.next = newNode;
+            }
+        }
+
+        
+        static void Remove(ref OneChain head, int value)
+        {
+            if (head == null)
+                return;
+
+            if (head.data == value)
+            {
+                head = head.next;
+                return;
+            }
+
+            OneChain current = head;
+            while (current.next != null)
+            {
+                if (current.next.data == value)
+                {
+                    current.next = current.next.next;
+                    return;
+                }
+                current = current.next;
+            }
+        }
+
+       
+        static void PrintList(OneChain head)
+        {
+            OneChain current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+            Console.WriteLine();
         }
     }
 }
